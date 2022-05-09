04f032d73724f8f051753c95c6352ffd2dca4333
Gabriel Cuin dos Santos <gabrielcuinprof@gmail.com>
2022-05-08 22:30:41 -0300

    Try catch

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,26 +1,32 @@
 import express from "express";
 import { SubmitFeedbackService } from "./services/submit-feedback-service";
 import { PrismaFeedbackRepositorie } from "./repositories/prisma/prisma-feedbacks-repositorie";
-import { NodemailerMailAdapter } from './adapters/nodeMailer/nodemailer-mail-adapter';
+import { NodemailerMailAdapter } from "./adapters/nodeMailer/nodemailer-mail-adapter";
 
 export const routes = express.Router();
 
 routes.post("/feedbacks", async (req, res) => {
-  const { type, comment, screenshot } = req.body;
+  try {
+    const { type, comment, screenshot } = req.body;
 
-  const prismaFeedbackRepositorie = new PrismaFeedbackRepositorie();
-  const nodemailerMailAdapter = new NodemailerMailAdapter();
+    const prismaFeedbackRepositorie = new PrismaFeedbackRepositorie();
+    const nodemailerMailAdapter = new NodemailerMailAdapter();
 
-  const submitFeedbackService = new SubmitFeedbackService(
-    prismaFeedbackRepositorie,
-    nodemailerMailAdapter
-  );
+    const submitFeedbackService = new SubmitFeedbackService(
+      prismaFeedbackRepositorie,
+      nodemailerMailAdapter
+    );
 
-  await submitFeedbackService.execute({
-    type,
-    comment,
-    screenshot,
-  });
+    await submitFeedbackService.execute({
+      type,
+      comment,
+      screenshot,
+    });
 
-  return res.status(201).send();
+    return res.status(201).send();
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).send();
+  }
 });
